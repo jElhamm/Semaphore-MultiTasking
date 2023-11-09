2b1887d6b5d27030949a685ad3c2f93a590df939
jelhamm <elhamjafari200312@gmail.com>
2023-11-09 18:49:13 +0330

    Add (InterfaceFork) with pickUp() and putDown() methods

---
+++ Dining_Philosophers/Implementation with Java/InterfaceFork.java
@@ -0,0 +1,10 @@
+/**
+ * This interface represents a fork and defines the methods for picking up and putting down the fork.
+ */
+
+ 
+
+public interface InterfaceFork {
+    void pickUp();
+    void putDown();
+}
